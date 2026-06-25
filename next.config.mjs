8955ffe56791786b3584870474572af394ec1503
James Harcourt <73539072+jdharcourt@users.noreply.github.com>
2026-06-25 15:22:20 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/ig",
+        destination: "/?utm_source=instagram&utm_medium=social",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
